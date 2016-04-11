7bd1646b1ef95bb40458dd227c4b8a1e6612d151
ric mclaughlin <ric@mclaughlin.today>
2016-04-10 17:52:04 -0700

    login, logout & header

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -5,9 +5,48 @@
     .module('app.auth')
     .controller('AuthController', AuthController);
 
-  function AuthController() {
-    var vm = this;
+  AuthController.$inject = ['$location', '$firebaseAuth'];
 
+  function AuthController($location, $firebaseAuth) {
+    var vm = this;
+    var firebaseReference = new Firebase('https://rm-wait-eat2.firebaseio.com/');
+    var firebaseAuthObject = $firebaseAuth(firebaseReference);
+
+    vm.register = register;
+    vm.login = login;
+    vm.logout = logout;
+
+    vm.user = {
+      email: '',
+      password: ''
+    };
+
+    function register(user) {
+      return firebaseAuthObject.$createUser(user)
+        .then(function () {
+          vm.login(user);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+
+    function login(user) {
+      return firebaseAuthObject.$authWithPassword(user)
+        .then(function (loggedInUser) {
+          console.log(loggedInUser);
+          $location.path('/waitlist');
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+    }
+
+    function logout() {
+      firebaseAuthObject.$unauth();
+      $location.path('/');
+    }
 
   }
 })();
